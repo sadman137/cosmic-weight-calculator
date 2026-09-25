3da9ee5e55cdafbd66baa119b6c03b423c424789
sadman137 <2008.sadman@gmail.com>
2026-09-25 16:57:51 +0600

    add planet dataset, astronaut and planets grid render

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1. Planet Dataset
+const planets = [
+    { name: "MOON", gravity: 0.166, file: "MOON.png", jumpHeight: -140, duration: 1.8 },
+    { name: "MERCURY", gravity: 0.38, file: "MERCURY.png", jumpHeight: -90, duration: 1.2 },
+    { name: "VENUS", gravity: 0.91, file: "VENUS.png", jumpHeight: -55, duration: 0.8 },
+    { name: "MARS", gravity: 0.38, file: "MARS.png", jumpHeight: -90, duration: 1.2 },
+    { name: "JUPITER", gravity: 2.36, file: "JUPITER.png", jumpHeight: -20, duration: 0.4 },
+    { name: "SATURN", gravity: 0.92, file: "SATURN.png", jumpHeight: -54, duration: 0.8 },
+    { name: "URANUS", gravity: 0.89, file: "URANUS.png", jumpHeight: -56, duration: 0.85 },
+    { name: "NEPTUNE", gravity: 1.12, file: "NEPTUNE.png", jumpHeight: -45, duration: 0.7 },
+];
+
+const astronautImagePath = "assets/ASTRONAUT.png";
+
+// 2. Render Planet Grid
+function renderPlanetCards() {
+    const grid = document.getElementById('planetsGrid');
+    if (!grid) return;
+    grid.innerHTML = '';
+
+    planets.forEach((planet, index) => {
+        const item = document.createElement('div');
+        item.className = 'planet-item';
+        item.setAttribute('data-planet', planet.name);
+
+        item.innerHTML = `
+            <div class="jumper-container">
+                <img id="jumper-${index}" class="astronaut-img" src="${astronautImagePath}" alt="Astronaut">
+            </div>
+            <div class="planet-visual-container">
+                <img class="planet-img" src="assets/${planet.file}" alt="${planet.name}">
+            </div>
+            <div class="planet-name">${planet.name}</div>
+            <div class="planet-weight-display" id="weight-${index}>--</div>  
+        `;
+
+        grid.appendChild(item);
+    });
+}
+
+window.onload = () => {
+    renderPlanetCards();
+};
